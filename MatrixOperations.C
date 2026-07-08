c1a7ea4d68b1f8e7051fefa34882a8df2a8bd129
saispurthikancharla <saispurthikancharla@gmail.com>
2026-07-08 12:42:32 +0530

    Add files via upload

---
+++ MatrixOperations.C
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+void add(int a[10][10], int b[10][10], int r, int c)
+{
+    int i, j;
+    printf("\nMatrix Addition:\n");
+    for(i=0; i<r; i++)
+    {
+        for(j=0; j<c; j++)
+            printf("%d ", a[i][j] + b[i][j]);
+        printf("\n");
+    }
+}
+
+void multiply(int a[10][10], int b[10][10], int r, int c)
+{
+    int i, j, k, m[10][10];
+
+    printf("\nMatrix Multiplication:\n");
+    for(i=0; i<r; i++)
+    {
+        for(j=0; j<c; j++)
+        {
+            m[i][j] = 0;
+            for(k=0; k<c; k++)
+                m[i][j] += a[i][k] * b[k][j];
+
+            printf("%d ", m[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void transpose(int a[10][10], int r, int c)
+{
+    int i, j;
+
+    printf("\nTranspose of Matrix A:\n");
+    for(i=0; i<c; i++)
+    {
+        for(j=0; j<r; j++)
+            printf("%d ", a[j][i]);
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int a[10][10], b[10][10];
+    int r, c, i, j;
+
+    printf("Enter rows and columns: ");
+    scanf("%d%d", &r, &c);
+
+    printf("Enter Matrix A:\n");
+    for(i=0; i<r; i++)
+        for(j=0; j<c; j++)
+            scanf("%d", &a[i][j]);
+
+    printf("Enter Matrix B:\n");
+    for(i=0; i<r; i++)
+        for(j=0; j<c; j++)
+            scanf("%d", &b[i][j]);
+
+    add(a, b, r, c);
+    multiply(a, b, r, c);
+    transpose(a, r, c);
+
+    return 0;
+}
